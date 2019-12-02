726f77eae6e206b51b2589d48bb91e1b78d3af74
mfrench <mfrench@indeed.com>
2019-12-02 00:03:06 -0600

    Cleaned up code a bit

--- src/day_2.rs
+++ src/day_2.rs
@@ -1,5 +1,4 @@
 mod utility;
-use std::collections::HashMap;
 use utility::stopwatch::stopwatch::*;
 use utility::utility::*;
 
@@ -8,78 +7,58 @@ fn part_2() {
     let stopwatch = Stopwatch::start_new();
     let original_ids = split_by_into_numbers(input, ",".parse().unwrap());
     let mut ids;
-    let targetOutput = 19690720;
+    let target_output = 19690720;
 
-    let mut closestResult = -1;
-    let mut closestValue1 = -1;
-    let mut closestValue2 = -1;
+    let mut closest_result = -1;
+    let mut closest_value1 = -1;
+    let mut closest_value2 = -1;
     let mut value1 = 0;
-    let value1Max = 99;
+    let value1max = 99;
     let mut value2 = 0;
-    let value2Max = 99;
+    let value2max = 99;
     loop {
         ids = original_ids.clone();
         ids[1] = value1;
         ids[2] = value2;
-        let mut position = 0;
-        'outer: loop {
-            let instruction = ids[position];
-            match instruction {
-                1 => {
-                    let part1index = ids[position + 1] as usize;
-                    let part2index = ids[position + 2] as usize;
-                    let part1 = ids[part1index];
-                    let part2 = ids[part2index];
-                    let added = part1 + part2;
-                    let part3 = ids[position + 3] as usize;
-                    ids[part3] = added;
-                    position += 4;
-                }
-                2 => {
-                    let part1index = ids[position + 1] as usize;
-                    let part2index = ids[position + 2] as usize;
-                    let part1 = ids[part1index];
-                    let part2 = ids[part2index];
-                    let added = part1 * part2;
-                    let part3 = ids[position + 3] as usize;
-                    ids[part3] = added;
-                    position += 4;
-                }
-                _ => {
-                    break 'outer;
-                }
+        run_instructions(ids.as_mut());
+        if closest_result == -1
+            || (ids[0] - target_output).abs() < (closest_result - target_output).abs()
+        {
+            closest_result = ids[0];
+            closest_value1 = value1;
+            closest_value2 = value2;
+            if (ids[0] - target_output).abs() == 0 {
+                break;
             }
         }
-        if closestResult == -1
-            || (ids[0] - targetOutput).abs() < (closestResult - targetOutput).abs()
-        {
-            closestResult = ids[0];
-            closestValue1 = value1;
-            closestValue2 = value2;
-        }
         value1 += 1;
-        if value1 > value1Max {
+        if value1 > value1max {
             value2 += 1;
             value1 = 0;
-            if value2 > value2Max {
+            if value2 > value2max {
                 break;
             }
         }
     }
     stopwatch.print_elapsed();
-    println!("Distance: {}", closestResult - targetOutput);
-    println!("Closest Result: {}", closestResult);
-    println!("Closest value 1: {}", closestValue1);
-    println!("Closest value 2: {}", closestValue2);
+    println!("Distance: {}", closest_result - target_output);
+    println!("Closest Result: {}", closest_result);
+    println!("Closest value 1: {}", closest_value1);
+    println!("Closest value 2: {}", closest_value2);
 }
 
 fn part_1() {
     let input = load_file("resources/day_2_input.txt");
     let stopwatch = Stopwatch::start_new();
     let mut ids = split_by_into_numbers(input, ",".parse().unwrap());
+    run_instructions(ids.as_mut());
+    stopwatch.print_elapsed();
+    println!("Result: {}", ids[0]);
+}
 
+fn run_instructions(ids: &mut Vec<i64>) {
     let mut position = 0;
-    'outer: loop {
+    loop {
         let instruction = ids[position];
         match instruction {
             1 => {
@@ -103,12 +82,10 @@ fn part_1() {
                 position += 4;
             }
             _ => {
-                break 'outer;
+                break;
             }
         }
     }
-    stopwatch.print_elapsed();
-    println!("Result: {}", ids[0]);
 }
 
 fn main() {
